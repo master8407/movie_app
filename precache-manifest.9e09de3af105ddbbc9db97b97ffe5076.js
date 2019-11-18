7ebbb7f325965effc8753a4a32f912e55e57f021
hoyoung byon <bhy8407@gmail.com>
2019-11-18 21:44:13 +0900

    Updates

---
+++ precache-manifest.9e09de3af105ddbbc9db97b97ffe5076.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57e1f6452b590d8396ffaac0c80ae6bc",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "af7abe7151c3c5402bdd",
+    "url": "/movie_app/static/css/main.85a35cda.chunk.css"
+  },
+  {
+    "revision": "dd26cfe0aeac87999839",
+    "url": "/movie_app/static/js/2.6e26b474.chunk.js"
+  },
+  {
+    "revision": "af7abe7151c3c5402bdd",
+    "url": "/movie_app/static/js/main.3dd45eb9.chunk.js"
+  },
+  {
+    "revision": "9b07edc508fa4bfefff7",
+    "url": "/movie_app/static/js/runtime-main.ab878cc8.js"
+  }
+]);
